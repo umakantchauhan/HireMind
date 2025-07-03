bd13dbe7e69974a418c121d74c6308718ef39cfa
umakantchauhan <ukchauhan369@gmail.com>
2025-07-03 17:05:07 +0530

    making authentation page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -45,22 +45,7 @@ const AuthForm = () =>{
                 <h3>Be interview prepared with AI</h3>
                     <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
-                <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                        This is your public display name.
-                    </FormDescription>
-                    <FormMessage />
-                    </FormItem>
-                )}
-                />
+                
                 <Button type="submit">Submit</Button>
             </form>
             </Form>

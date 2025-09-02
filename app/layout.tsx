aa44de5e9c8a13ba5f2ffcb9ad60f3a94bc5797f
umakantchauhan <ukchauhan369@gmail.com>
2025-09-02 20:25:48 +0530

    m

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,20 +7,15 @@ const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "HireMind",
   description: "An ai powered platform for preparing interviews",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-
-  
   return (
     <html lang="en" className="dark">
       <body className={`${monaSans.className} antialiased pattern`}>

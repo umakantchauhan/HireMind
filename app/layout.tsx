a65c93056abf8d1e568e1949ab582bd3f20554ab
umakantchauhan <ukchauhan369@gmail.com>
2025-09-04 20:34:11 +0530

    m

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   title: "HireMind",
   description: "An ai powered platform for preparing interviews",
 };
+
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
